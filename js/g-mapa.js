fa766d355965bcbfc7541a9a2709b63913c1f9f9
Roman Danko <mail@romandanko.sk>
2016-12-07 02:05:55 +0100

    -vytvoreny adresar html/ pre podstranky -vsetky html ine ako index.html presunute do html/ -pridana mapka slovenska(google map verzia) -upravene menu - pridana mapka

---
+++ js/g-mapa.js
@@ -0,0 +1,38 @@
+/**
+ * Created by rdanko on 07.12.2016.
+ */
+
+var map, transitLayer, infowindow;
+
+function initializeMap() {
+    //vytvorime mapu
+    var mapCanvas = document.getElementById("map");
+    var mapOptions = {
+        center: new google.maps.LatLng(48.74, 19.68),
+        zoom: 8
+    };
+
+    map = new google.maps.Map(mapCanvas, mapOptions);
+
+    //vykresli markery pre pamiatky z ext. JSON suboru. Realizovane pomocou jQuery funkcie
+    $.getJSON( "../data/pamiatky.json", function( data ) {
+
+        console.log(data);
+        $.each( data.pamiatky, function( key, pam ) {
+
+            alert(pam.nazov);
+            var pos = new google.maps.LatLng(pam.sirka,pam.dlzka);
+            var marker = new google.maps.Marker({position: pos});
+
+            infowindow = new google.maps.InfoWindow({
+                content: pam.nazov + "\n" + pam.rokVzniku
+            });
+            infowindow.open(map,marker);
+        });
+
+
+    });
+
+}
+
+google.maps.event.addDomListener(window, "load", initializeMap);
--- js/menu.js
+++ js/menu.js
@@ -2,8 +2,9 @@ function generuj_menu() {
     var level1 = [
         {name : "Úvod", typ:0, ref:"index.html"},
         {name : "Technické pamiatky", typ:1, ref:""},
-        {name : "Kalendár", typ:0, ref:"meniny.html"},
-        {name : "Pripomienkovač", typ:0, ref:"reminder.html"},
+        {name : "Mapa", typ:0, ref:"html/g-mapa.html"},
+        {name : "Kalendár", typ:0, ref:"html/meniny.html"},
+        {name : "Pripomienkovač", typ:0, ref:"html/reminder.html"},
         {name : "Hry", typ:1, ref:""},
         {name : "Autori", typ:1, ref:""}];
 
@@ -20,8 +21,8 @@ function generuj_menu() {
         {name:"Vysutá lanová dráha", typ:0, ref:"", autor:"Adam Podhradský"},
         {name:"Gápeľ-Solivar Prešov", typ:0, ref:"", autor:"Erik Pribula"},
         {name:"Hvezdáreň Prešov", typ:0, ref:"", autor:"Erik Pribula"},
-        {name:"Zvolenský hrad", typ:0, ref:"pamiatky-ls.html", autor:"Lukáš Šníder"},
-        {name:"Štôlňa-Banská Štiavnica", typ:0, ref:"pamiatky-ls.html", autor:"Lukáš Šníder"},
+        {name:"Zvolenský hrad", typ:0, ref:"html/pamiatky-ls.html", autor:"Lukáš Šníder"},
+        {name:"Štôlňa-Banská Štiavnica", typ:0, ref:"html/pamiatky-ls.html", autor:"Lukáš Šníder"},
         {name:"Jazero mieru", typ:0, ref:"", autor:"Martin Meľuch"},
         {name:"Slávikov mlyn", typ:0, ref:"", autor:"Martin Meľuch"},
     ];
