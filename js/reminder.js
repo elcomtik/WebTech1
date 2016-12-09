05817e20b286544c51734bcd6b0d8e77c04db423
Lukas Snider <lukassnider@gmail.com>
2016-12-09 23:35:14 +0100

    Dokoncil som pripomienkovac - ukladanie do localStore, load z neho, tooltip pri zlom vyplneni formulara.

--- js/reminder.js
+++ js/reminder.js
@@ -1,29 +1,21 @@
 function saveNote(){
     if (document.getElementById("note-name").value == ""){
-        //TODO: Tooltip or something...
-        console.log("EMPTY Title");
+        $('#note-name').css('border-color', 'red');
+        $('#note-name').tooltip('show');
     }
     else if (document.getElementById("note-content").value == "") {
-        //TODO: Tooltip or something...
-        console.log("EMPTY Content");
+        $('#note-content').css('border-color', 'red');
+        $('#note-content').tooltip('show');
     }
     else {
-        // Adds a new reminder-notes div element with use of temporary name (suffix -new) in class name
-        $("div.reminder-notes").append('<div class="item-new"></div>');
-        $notesitem = $("div.item-new");
+        //Clear styles of previously false input if there was any
+        $('#note-name').css('border-color', '');
+        $('#note-name').tooltip('destroy');
+        $('#note-content').css('border-color', '');
+        $('#note-content').tooltip('destroy');
 
-        // The x close sign
-        $notesitem.append('<span class="close" onclick="deleteNote(this)">&times;</span>');
-
-        $notesitem.append('<div class="title-new"></div>');
-        $(".title-new").text($("input#note-name").val());
-        $(".title-new").attr("class", "panel-heading");
-
-        $notesitem.append('<div class="content-new"></div>');
-        $(".content-new").text($("textarea#note-content").val());
-        $(".content-new").attr("class", "panel-body");
-
-        $notesitem.attr("class", "panel panel-default");
+        var newItem = storeNewNote();
+        addNoteToPage(newItem.id, newItem.name, newItem.content);
 
         // Clear the input field and textarea
         $('input#note-name').val('');
@@ -31,7 +23,47 @@ function saveNote(){
     }
 }
 
+function addNoteToPage(noteId, noteName, noteContent) {
+    // Adds a new reminder-notes div element with use of temporary name (suffix -new) in class name
+    $("div.reminder-notes").append('<div class="item-new"></div>');
+    $notesitem = $("div.item-new");
+
+    // The x close sign
+    $notesitem.append('<span class="close" onclick="deleteNote(this)">&times;</span>');
+
+    $notesitem.attr("id", noteId);
+    $notesitem.append('<div class="title-new"></div>');
+    $(".title-new").text(noteName);
+    $(".title-new").attr("class", "panel-heading");
+    $notesitem.append('<div class="content-new"></div>');
+    $(".content-new").text(noteContent);
+    $(".content-new").attr("class", "panel-body");
+
+    $notesitem.attr("class", "panel panel-default");
+}
+
+// Stores new note to localStore
+function storeNewNote() {
+    var ni = {'id': 0, 'name': '', 'content': ''};
+
+    ni.name = $("input#note-name").val();
+    ni.content = $("textarea#note-content").val();
+    ni.id = new Date().getTime();
+    localStorage.setItem(String(ni.id), JSON.stringify(ni));
+
+    return ni;
+}
+
+// This is called from x close sign element
 function deleteNote(location){
-    // This is called from x close sign element
     location.parentElement.remove();
+    localStorage.removeItem(location.parentElement.getAttribute('id'));
+}
+
+// Loads the notes from localStore
+window.onload = function() {
+    for(var i in localStorage) {
+        addNoteToPage(JSON.parse(localStorage.getItem(i)).id,
+          JSON.parse(localStorage.getItem(i)).name, JSON.parse(localStorage.getItem(i)).content);
+    }
 }
