0930510ee1984d22edccd26f0bdaae2997280387
Roman Danko <mail@romandanko.sk>
2016-12-10 17:11:38 +0100

    upraveny nazov mojej hry v menu

--- js/menu.js
+++ js/menu.js
@@ -59,7 +59,7 @@ function generuj_menu() {
         {name: "Slávikov mlyn", typ: 0, ref: "/html/pamiatky.html#pam_martin", autor: "Martin Meľuch"},
     ];
     var hry = [
-        {name:"Čínske šachy", typ:0, ref:"/html/vynalezcovia.html", autor:"Roman Danko"},
+        {name:"Priraď vynález", typ:0, ref:"/html/vynalezcovia.html", autor:"Roman Danko"},
         {name:"Čísla", typ:0, ref:"/html/numbers.html", autor:"Adam Podhradský"},
         {name:"Hanojské veže", typ:0, ref:"/html/veze.html",autor:"Erik Pribula"},
         {name:"Zemepisné puzzle", typ:0, ref:"/html/geopuzzle.html", autor:"Lukáš Šníder"},
